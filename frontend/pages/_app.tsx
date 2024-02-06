665d73eee85ebfa18264d67db9b60ce9ac30e82c
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2024-02-06 15:29:43 +0900

    feta(app): recoil 및 toast 라이브러리 추가

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,22 @@
 import AppLayout from "@/components/layout/AppLayout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+import { ToastContainer } from "react-toastify";
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AppLayout>
-      <Component {...pageProps} />
-    </AppLayout>
+    <RecoilRoot>
+      <ToastContainer />
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    </RecoilRoot>
   );
 }
